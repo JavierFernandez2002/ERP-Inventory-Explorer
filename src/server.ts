551a06124443c9810387f06c94bfaefd7580fe18
javfernandezfrba <javfernandez@frba.utn.edu.ar>
2026-03-15 15:41:05 -0300

    feat: add Redis client configuration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
-import app from './app';
+import app from "./app";
+import { redisClient } from "./config/redis";
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
+async function startServer() {
+  try {
+    if (!redisClient.isOpen) {
+      await redisClient.connect();
+    }
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+}
+
+startServer();
